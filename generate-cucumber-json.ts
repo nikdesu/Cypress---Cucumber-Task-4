41fb7f28150fd1151cb42dbe61466fffeaf297cc
nikdesu <ostspchyk1409@gmail.com>
2023-11-15 19:04:06 +0200

    commit

--- generate-cucumber-json.ts
+++ generate-cucumber-json.ts
@@ -1,10 +1,10 @@
 import { Formatter } from "cucumber-json-report-formatter";
 
-async function reportCreate() {
+async function test() {
   const formatter = new Formatter();
   const sourceFile = "./jsonlogs/messages.ndjson";
   const outputFile = "./reports/cucumber-report.json";
   await formatter.parseCucumberJson(sourceFile, outputFile);
 }
 
-reportCreate();
+test();
