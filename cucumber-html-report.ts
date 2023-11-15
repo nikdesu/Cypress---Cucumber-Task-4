bd56ad6670afed70cf6b28233b82dbb780acba18
nikdesu <ostspchyk1409@gmail.com>
2023-11-15 19:36:40 +0200

    commit

--- cucumber-html-report.ts
+++ cucumber-html-report.ts
@@ -1,6 +1,6 @@
 const report = require("multiple-cucumber-html-reporter");
 report.generate({
-  jsonDir: ".",
+  jsonDir: "./",
   reportPath: "./cucumber-report.html",
   metadata: {
     browser: {
